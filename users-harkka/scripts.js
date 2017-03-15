bbb8b5e5479e92421ccc1cf24d783d5f892a67c4
JulParman <julian.parman@gmail.com>
2017-03-15 10:49:26 +0200

    Fixes

--- users-harkka/scripts.js
+++ users-harkka/scripts.js
@@ -1,24 +1,41 @@
 /**
  * Created by ekoodi on 14.3.2017.
  */
-var users = [];
+var contacts = [];
+var newContact;
 
-function addUser() {
-    var firstName = document.getElementById('txtfname');
-    var lastName = document.getElementById('txtlname');
-    var age = document.getElementById('txtage');
-    var address = document.getElementById('txtaddress');
-    var newUser = {fName: firstName, lName: lastName, a: age, add: address};
-    users.push(newUser);
-    console.log(users);
+
+function addContact() {
+    newContact = createNewContactObject();
+    contacts.push(newContact);
+    console.log(contacts);
+    getContact(contacts);
+    console.log(contacts);
 }
 
-function getUsers() {
-    for (var i = 0; i < users.length; i++) {
-        document.write('<tr><th>' + users[i.fName] + '</th>');
-        document.write('<th>' + users[i.lName] + '</th>');
-        document.write('<th>' + users[i.a] + '</th>');
-        document.write('<th>' + users[i.add] + '</th></tr>');
+function createNewContactObject() {
+    var firstName = document.getElementById('txtfname').value;
+    var lastName = document.getElementById('txtlname').value;
+    var age = document.getElementById('txtage').value;
+    var address = document.getElementById('txtaddress').value;
+
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+        address: address
+    };
+}
+
+function getContact(contacts) {
+    var newRow = '';
+    for (var i = 0; i < contacts.length; i++) {
+        newRow += '<tr><td>' + contacts[i].firstName + '</td>' +
+        '<td>' + contacts[i].lastName + '</td>' +
+        '<td>' + contacts[i].age + '</td>' +
+        '<td>' + contacts[i].address + '</td></tr>'
     }
+    var table = document.getElementById('utable');
+    table.innerHTML = newRow;
 
 }
