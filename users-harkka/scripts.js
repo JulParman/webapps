8c3cc844a77668c6cb09da05195dc70a9574776a
JulParman <julian.parman@gmail.com>
2017-03-15 14:36:17 +0200

    Delete button added

--- users-harkka/scripts.js
+++ users-harkka/scripts.js
@@ -4,38 +4,90 @@
 var contacts = [];
 var newContact;
 
+function loadData() {
+    try{
+        contacts = getLocalStorage();
+    }
+    catch(err) {
+        document.getElementById("header3").innerHTML = err.message;
+    }
+    if (contacts===null)
+    {
+
+    }
+    else
+    {
+        getContact(contacts);
+    }
+}
+
 
 function addContact() {
-    newContact = createNewContactObject();
-    contacts.push(newContact);
-    console.log(contacts);
-    getContact(contacts);
-    console.log(contacts);
+
+        newContact = createNewContactObject();
+        contacts.push(newContact);
+        writeLocalStorage(contacts);
+        getContact(contacts);
 }
 
 function createNewContactObject() {
     var firstName = document.getElementById('txtfname').value;
     var lastName = document.getElementById('txtlname').value;
-    var age = document.getElementById('txtage').value;
+    var phone = document.getElementById('txtphone').value;
     var address = document.getElementById('txtaddress').value;
 
     return {
         firstName: firstName,
         lastName: lastName,
-        age: age,
+        phone: phone,
         address: address
     };
 }
 
-function getContact(contacts) {
+function getContact() {
+    contacts = getLocalStorage();
     var newRow = '';
     for (var i = 0; i < contacts.length; i++) {
-        newRow += '<tr><td>' + contacts[i].firstName + '</td>' +
-        '<td>' + contacts[i].lastName + '</td>' +
-        '<td>' + contacts[i].age + '</td>' +
-        '<td>' + contacts[i].address + '</td></tr>'
+        newRow += "<tr><td>" + contacts[i].firstName + "</td>" +
+        "<td>" + contacts[i].lastName + "</td>" +
+        "<td>" + contacts[i].phone + "</td>" +
+        "<td>" + contacts[i].address + "</td></tr>"
     }
-    var table = document.getElementById('utable');
-    table.innerHTML = newRow;
+    var tableElement = document.getElementById('utable');
+    tableElement.innerHTML = newRow;
 
+}
+
+function writeLocalStorage(contacts){
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("contacts",JSON.stringify(contacts));
+
+    } else {
+        document.getElementById("header3").innerHTML = "Ei toimi";
+    }
+}
+
+function getLocalStorage(){
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        contacts = JSON.parse(localStorage.getItem("contacts"));
+    } else {
+        document.getElementById("header3").innerHTML = "Ei toimi";
+    }
+    return contacts;
+}
+
+function deleteRow(){
+    contacts = getLocalStorage();
+    var firstName = document.getElementById('txtfname').value;
+    var lastName = document.getElementById('txtlname').value;
+    for(var i = 0;i<contacts.length;i++)
+    {
+        if(contacts[i].firstName === firstName)
+        {
+            contacts.splice(i,4);
+        }
+    }
+    writeLocalStorage(contacts);
 }
